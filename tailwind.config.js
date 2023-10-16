f7e0345de40b283994403566bd130ef59c58e120
ming <matwming114@gmail.com>
2023-10-16 14:55:48 +1100

    fix(tailwind.config): update tailwind config safelist and borderRadius

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        "1rem": "1rem",
+      },
+    },
   },
   plugins: [],
-}
+  safelist: [
+    "bg-[#FBEA9F]",
+    "bg-[#C7EAF0]",
+    "bg-[#ffb763]",
+    "bg-[#FCA5A5]",
+    "bg-[#E5D9FF]",
+    "bg-[#FFCBDE]",
+  ],
+};
